bc03e718a4d1bc831932a489c891e0a0618302af
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2025-08-03 15:19:18 +0530

    Tailwind Setup & Controlled Inputs

---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+
+const NoteForm = () => {
+  const [title, setTitle] = useState('')
+  const [priority, setPriority] = useState('medium')
+  const [category, setCategory] = useState('work')
+  const [description, setDescription] = useState('')
+
+  return (
+    <form className='mb-6'>
+      <div className='mb-4'>
+        <label htmlFor='title' className='block font-semibold text-left'>
+          Title
+        </label>
+        <input
+          type='text'
+          className='w-full p-2 border rounded-lg'
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+      </div>
+      <div className='mb-4'>
+        <label htmlFor='priority' className='block font-semibold text-left'>
+          Priority
+        </label>
+        <select
+          type='text'
+          className='w-full p-2 border rounded-lg'
+          value={priority}
+          onChange={e => setPriority(e.target.value)}
+        >
+          <option value='high'>🔴 High</option>
+          <option value='medium'>🟡 Medium</option>
+          <option value='low'>🟢 Low</option>
+        </select>
+      </div>
+      <div className='mb-4'>
+        <label htmlFor='category' className='block font-semibold text-left'>
+          Category
+        </label>
+        <select
+          type='text'
+          className='w-full p-2 border rounded-lg'
+          value={category}
+          onChange={e => setCategory(e.target.value)}
+        >
+          <option value='work'>📁 Work</option>
+          <option value='Personal'>🏠 Personal</option>
+          <option value='Ideas'>💡 Ideas</option>
+        </select>
+      </div>
+      <div className='mb-4'>
+        <label htmlFor='description' className='block font-semibold text-left'>
+          Description
+        </label>
+        <textarea
+          type='text'
+          className='w-full p-2 border rounded-lg'
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        ></textarea>
+      </div>
+      <button className='w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600'>
+        Add Note
+      </button>
+    </form>
+  )
+}
+
+export default NoteForm
